677c89b9d82a4c4874a05546e19aa357905b6e23
ItSolution21 <161233126+ItSolution21@users.noreply.github.com>
2024-03-06 13:13:37 +0530

    Add files via upload

---
+++ five.js
@@ -0,0 +1,115 @@
+
+// // // 
+// // let a = 23 
+// // console.log(a)
+
+// // let b = 2;
+// // let c = 4;
+
+// // console.log(b+c)
+
+// // //arithmetic operators (+, -, *, /, %)
+
+// // console.log(2+3)
+// // console.log(11%5)
+
+// // console.log(2+3+4+5+6)         //20
+// // console.log(2+3+4+5+''+6)       //146
+// // console.log(2+3+4+''+5+6)      //956
+// // console.log(2+3+''+4+5+6)      //5456
+// // console.log(2+''+3+4+5+6)       //23456
+// // console.log(''+2+3+4+5+6)       //23456
+
+// // console.log(2 ** 3);
+
+// // //assigment operator = storing and mainpulate at the same time 
+
+// // let ab = 23;
+
+// // // ab = ab + 4;
+// // ab += 4;
+
+// // ab += 3;
+// // console.log(ab)
+
+// // //comparign operator = comparing one or two values and get the output as true or false
+// // // (=, ==, ===, !=, !==, >, <, >=, >==, <=, <==)
+// console.log(5 == '5');
+// console.log(5 === 5);
+
+// //logical operators 
+
+console.log(true && true); //true
+console.log(true && false) // false
+console.log(false && false) //false 
+console.log(false && true) //false
+console.log("***********************************")
+console.log((2>=30) && (4<2));
+
+console.log(((2<=30) && (4>2)) && true);
+
+//or operator
+console.log("***********************************")
+console.log(true || true); 
+console.log(true || false); 
+console.log(false || false); 
+console.log(false || true);
+
+console.log("***********************************")
+console.log((2>=34) || (2<4));
+console.log(((2>=34) || (2<4)) || false);
+
+console.log((((2>=34) || (2>4)) || false) || false);
+console.log("***********************************")
+
+console.log(true)
+console.log(!true)
+console.log("***********************************")
+console.log(!(!false && true))
+
+console.log("***********************************")
+console.log(!!!true)
+
+let a = 23;
+let b = 2;
+let c = 4;
+console.log("***********************************")
+
+console.log(!(!!!(!(a == b)) && (c == b)));
+console.log("***********************************")
+
+// var ax = 23;
+// ax = ax + 4;
+// ax = ax + 23;
+// if(true){
+//     ax = ax + 3;
+// }
+// if(true){
+//     ax = ax + 3;
+// }
+// console.log(ax)
+
+
+// console.log("**************************************")
+
+// let bx = 23;
+// bx = bx + 4;
+// if(true){
+//     let bx = 2;
+//     console.log(bx);
+// }
+// console.log(bx)
+
+// console.log("**************************************")
+
+// const cx = 23;
+// // cx = cx + 4;
+// console.log(cx)
+
+console.log("**************************************")
+
+console.log(true && 'suppose');
+console.log('suppose' && true);
+console.log('suppose' && false);
+console.log(false && 'suppose');
+console.log(true || 'suppose');
